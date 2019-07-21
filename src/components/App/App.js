640a9477714d993a72217ebe474748e2b2b7cb14
Andrea Beltramello <andreabeltramello@Andreas-MacBook-Pro.local>
2019-07-20 23:04:51 -0600

    What is going on

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          WOOOoooooooow
+          yo what the hell is going on???
         </p>
       </header>
     </div>
